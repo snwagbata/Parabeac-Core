a4aa2fd3faa156ce5f229213cfb2092b63c18069
Ivan Vigliante <ivan.avr@protonmail.com>
2020-11-08 16:58:30 -0500

    Added stack reduction post-rule that is triggered when a stack has a container that should wrap the other element of the stack

--- lib/interpret_and_optimize/entities/layouts/rules/container_rule.dart
+++ lib/interpret_and_optimize/entities/layouts/rules/container_rule.dart
@@ -12,13 +12,17 @@ class ContainerPostRule extends PostConditionRule {
     overlappingNodesLayoutRule = OverlappingNodesLayoutRule();
   }
 
+  /// Returns true if `currentNode` is a [PBIntermediateStackLayout] with two
+  /// children: a [PBVisualIntermediateNode] and [PBLayoutIntermediateNode] that
+  /// have overlapping coordinates.
   @override
   bool testRule(PBIntermediateNode currentNode, PBIntermediateNode nextNode) {
     var layout =
         currentNode is PBIntermediateStackLayout ? currentNode : nextNode;
-    if (layout == null) {
+    if (layout == null || layout is! PBIntermediateStackLayout) {
       return false;
     }
+
     var children = (layout as PBLayoutIntermediateNode).children;
     if (children.length == 2) {
       if ((children[0] is PBVisualIntermediateNode &&
@@ -36,6 +40,9 @@ class ContainerPostRule extends PostConditionRule {
     return false;
   }
 
+  /// Removes the [PBIntermediateStackLayout] and wraps the
+  /// [PBLayoutIntermediateNode] child with the [PBVisualIntermediateNode]
+  /// child.
   @override
   dynamic executeAction(
       PBIntermediateNode currentNode, PBIntermediateNode nextNode) {
